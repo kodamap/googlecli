bce6589f1c860fcbebf0f8b4702fa6383f00ddb9
kodamap <ooo@kodamap.net>
2019-07-13 15:19:52 +0900

    fix yes_no

--- googlectl/delete.py
+++ googlectl/delete.py
@@ -6,6 +6,7 @@ from cliff.command import Command
 
 import googlectl.libgooglectl
 
+
 class UserDelete(Command):
     "Delete the group"
 
@@ -18,9 +19,11 @@ class UserDelete(Command):
 
     def take_action(self, parsed_args):
         client = googlectl.libgooglectl.Client()
-        response = client.yes_or_no("Are you sure to delete {} ?".format(parsed_args.email))
+        response = client.yes_no(
+            "Are you sure to delete {} ?".format(parsed_args.email))
         if response:
             client.delete_user(parsed_args.email)
+
 
 class GroupDelete(Command):
     "Delete the group"
@@ -34,7 +37,8 @@ class GroupDelete(Command):
 
     def take_action(self, parsed_args):
         client = googlectl.libgooglectl.Client()
-        response = client.yes_or_no("Are you sure to delete {} ?".format(parsed_args.groupkey))
+        response = client.yes_no(
+            "Are you sure to delete {} ?".format(parsed_args.groupkey))
         if response:
             client.delete_group(parsed_args.groupkey)
 
@@ -47,12 +51,14 @@ class GroupMemberDelete(Command):
     def get_parser(self, prog_name):
         parser = super(GroupMemberDelete, self).get_parser(prog_name)
         parser.add_argument('email', nargs=None, default=None)
-        parser.add_argument('--groupkey', nargs=None, required=True, default=None)
+        parser.add_argument('--groupkey', nargs=None,
+                            required=True, default=None)
         return parser
 
     def take_action(self, parsed_args):
         client = googlectl.libgooglectl.Client()
-        response = client.yes_or_no("Are you sure to delete {} from {} ?".format(parsed_args.email, parsed_args.groupkey))
+        response = client.yes_no("Are you sure to delete {} from {} ?".format(
+            parsed_args.email, parsed_args.groupkey))
 
         if response:
             client.delete_member(parsed_args.groupkey, parsed_args.email)
